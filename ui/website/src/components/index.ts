e258ec582d9a96139f97c7aa072a4440b1b197ad
bojikian <bojikian@hu-berlin.de>
2026-04-20 23:52:58 +0200

    removed website files and deployment

--- ui/website/src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Filetree } from './Filetree';
-export { default as ASTview } from './astview/ASTview';
-export { default as ControlPanel } from './ControlPanel';
-export { default as Codeview } from './Codeview';
-export { default as TreeNode } from './astview/TreeNode';
